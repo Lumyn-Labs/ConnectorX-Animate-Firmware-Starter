64cf94db552fa1e32ad243572b5ef02424967e59
having11 <gatoninja236@gmail.com>
2025-02-02 07:26:26 +0000

    Push updated include

--- include/constants/ColorConstants.h
+++ include/constants/ColorConstants.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include "ConfigurationParser/Configuration.h"
-
 namespace ColorConstants {
 static const Configuration::ActionColor defaultAnimationColor = {0, 0, 240};
 }  // namespace ColorConstants
