6d8da39c292ecd3f2ea92fd450bab0b4e8fc8399
having11 <gatoninja236@gmail.com>
2025-11-18 08:20:30 +0000

    Push updated lib

--- lib/LumynLabs/include/ConfigurationParser/Configuration.h
+++ lib/LumynLabs/include/ConfigurationParser/Configuration.h
@@ -13,7 +13,7 @@
 #include "ConfigurationParser/configs/Bitmap.h"
 #include "ConfigurationParser/configs/Channel.h"
 #include "ConfigurationParser/configs/Network.h"
-#include "ConfigurationParser/configs/Sensor.h"
+#include "ConfigurationParser/configs/Module.h"
 #include "constants/board_variant.h"
 
 namespace Configuration {
